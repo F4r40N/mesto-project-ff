82e1a3bac741a2c820a31265eebe6c1d2f01065b
Evgeny Shadrin <vezu-63@yandex.ru>
2025-05-28 14:33:38 +0400

    review-1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+function createCard(initialCards) {
+    const cardTemplate = document.querySelector('#card-template').content.cloneNode(true);
+    
+    cardTemplate.querySelector('.card__title').textContent = initialCards.name;
+    cardTemplate.querySelector('.card__image').src = initialCards.link;
+    cardTemplate.querySelector('.card__image').alt = initialCards.name;
 
-// @todo: DOM узлы
+    const deleteButton = cardTemplate.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {
+        const cardToDelete = deleteButton.closest('.card');
+        deleteCard(cardToDelete);
+    });
 
-// @todo: Функция создания карточки
+    return cardTemplate;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(cardDelete) {
+    cardDelete.remove();
+}
 
-// @todo: Вывести карточки на страницу
+function renderCards(cardArray) {
+    const placesList = document.querySelector('.places__list');
+    
+    cardArray.forEach((initialCards) => {
+        const cardElement = createCard(initialCards);
+        placesList.append(cardElement);
+    });
+}
+
+renderCards(initialCards);
